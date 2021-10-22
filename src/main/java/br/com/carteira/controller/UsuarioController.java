ccfcfe507b229e9220c21cdfdffc0bddf9a1ca5d
andersonsouza90 <andersonsilva.souza90@gmail.com>
2021-10-22 00:29:05 -0300

    Documentação da API com Swagger

--- src/main/java/br/com/carteira/controller/UsuarioController.java
+++ src/main/java/br/com/carteira/controller/UsuarioController.java
@@ -20,20 +20,25 @@ import br.com.carteira.dto.TransacaoDto;
 import br.com.carteira.dto.UsuarioDto;
 import br.com.carteira.dto.UsuarioFormDto;
 import br.com.carteira.service.UsuarioService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 
 @RestController
 @RequestMapping("/usuarios")
+@Api(tags = "Usuario")
 public class UsuarioController {
 	
 	@Autowired
 	private UsuarioService service;
 	
 	@GetMapping
+	@ApiOperation("Listar usuarios")
 	public Page<UsuarioDto> listar(@PageableDefault(size = 10) Pageable paginacao) {
 		return service.listar(paginacao);
 	}
 	
 	@PostMapping
+	@ApiOperation("Cadastrar novo usuarios")
 	public ResponseEntity<UsuarioDto> cadastrar(@RequestBody @Valid UsuarioFormDto dto,
 							UriComponentsBuilder uriBuilder) {
 		
--- src/test/java/br/com/carteira/controller/UsuarioControllerTest.java
+++ src/test/java/br/com/carteira/controller/UsuarioControllerTest.java
@@ -16,6 +16,8 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import io.swagger.annotations.Api;
+
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
 @AutoConfigureMockMvc
